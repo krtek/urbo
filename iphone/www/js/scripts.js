582a70c1dfb84b1e771d49d06bca8aad6f1c4ddd
Lukas Lukovsky <lukas.lukovsky@gmail.com>
2012-07-24 16:20:03 +0200

    Fixed some git mystery

--- iphone/www/js/scripts.js
+++ iphone/www/js/scripts.js
@@ -62,7 +62,7 @@ function getPhoto(photoSourceType) {
                                 });
 }
 
-function setNewGuardReport() {
+function prepareNewMessage() {
     var photoDataView = document.getElementById('photoDataView')
     photoDataView.style.backgroundImage="";
     photoDataView.innerHTML = "Vybrat foto";
@@ -116,7 +116,7 @@ function getGpsCoordinates() {
     navigator.geolocation.getCurrentPosition(onGpsCoordsSuccess, onGpsCoordsError);
 }
 
-function adjustGpsCoords() {
+function adjustLocation() {
     if(urboItem.LocationLatitude != '') {
         showMapToAdjust('newMessageView', urboItem.LocationLatitude, urboItem.LocationLongitude);
     } else {
