7c23d56ae638f4b72162824d95bd9b5a153e41b9
Lukas Marek <lukas.marek@gmail.com>
2012-08-02 15:57:42 +0200

    First, fully working jQuery version

---
+++ android-jquery/src/cz/urbo/MainActivity.java
@@ -0,0 +1,17 @@
+package cz.urbo;
+
+import org.apache.cordova.DroidGap;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends DroidGap
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
